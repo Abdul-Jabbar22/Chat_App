e2bc2992fc614c079af24d88421289de02b36b75
Abdul Jabbar <123597574+Abdul-Jabbar22@users.noreply.github.com>
2024-06-25 19:28:26 +0500

    work on ui

--- src/components/detail/detail.jsx
+++ src/components/detail/detail.jsx
@@ -1,6 +1,42 @@
 import "./detail.css";
+
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Abbul Jabbar </h2>
+        <p>Loram ipsum dolar sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy % help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Share Photos</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <img src="" alt="" />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
